c849f02cab01d50344e68bd3d0573d6afecadfcd
Ana Coronel <a9nna.coronel@gmail.com>
2023-02-16 19:34:35 +0100

    Add the method to return a http.Server object

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello");
+import "./loadEnvironment.js";
+import express from "express";
+import morgan from "morgan";
+import createDebug from "debug";
+import chalk from "chalk";
+
+const app = express();
+const debug = createDebug("main-module");
+const port = process.env.PORT ?? 3784;
+
+app.use(morgan("dev"));
+
+app.listen(port, () => {
+  debug(chalk.bgGreen(`Start with server 'http://localhost:${port}'`));
+});
