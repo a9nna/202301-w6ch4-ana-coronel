58e635b8298abeed9fda050c4a36d1a0f2341fe8
Ana Coronel <a9nna.coronel@gmail.com>
2023-02-16 20:08:33 +0100

    Add data layer with the type of things already know

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,17 @@ import express from "express";
 import morgan from "morgan";
 import createDebug from "debug";
 import chalk from "chalk";
+import thingsAlredyKnow from "./thingsAlreadyKnow.js";
 
 const app = express();
 const debug = createDebug("main-module");
 const port = process.env.PORT ?? 3784;
 
 app.use(morgan("dev"));
+
+app.get("/things", (req, res) => {
+  res.status(200).json({ thingsAlredyKnow });
+});
 
 app.listen(port, () => {
   debug(chalk.bgGreen(`Start with server 'http://localhost:${port}'`));
---
+++ src/types.ts
@@ -0,0 +1,6 @@
+export interface ThingAlreadyKnow {
+  id: number;
+  area: string;
+  thingAlredyKnow: string;
+  levelOfKnowledge: "low" | "medium" | "high";
+}
