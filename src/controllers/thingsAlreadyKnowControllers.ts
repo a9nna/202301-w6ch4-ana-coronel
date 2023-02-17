c7a72d5b15fdde21038a9e7855a6b6d9e01ae793
Ana Coronel <a9nna.coronel@gmail.com>
2023-02-17 08:03:12 +0100

    Add a controller to get a single thingAlreadyKnow

--- src/controllers/thingsAlreadyKnowControllers.ts
+++ src/controllers/thingsAlreadyKnowControllers.ts
@@ -4,3 +4,10 @@ import thingsAlredyKnow from "../thingsAlreadyKnow.js";
 export const getThingsAlreadyKnow = (req: Request, res: Response) => {
   res.status(200).json({ thingsAlredyKnow });
 };
+
+export const getSingleThingAlreadyKnow = (req: Request, res: Response) => {
+  const id = req.params.idThing;
+  const thingToGet = thingsAlredyKnow.find((thing) => thing.id === +id);
+
+  res.status(200).json({ thingToGet });
+};
